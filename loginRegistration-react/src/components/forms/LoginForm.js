3e3d290a6e02b5b3f7547c2107ab2c5a9a8ca01f
Erick Lui <erickjin.lui@gmail.com>
2018-05-22 17:03:00 -0700

    added loading state on submit

--- loginRegistration-react/src/components/forms/LoginForm.js
+++ loginRegistration-react/src/components/forms/LoginForm.js
@@ -30,9 +30,12 @@ class LoginForm extends Component {
     // pass to submit function and return promise
     // if no errors, pass data to submit action
     if (Object.keys(errors).length === 0) {
+      this.setState({ loading: true });
       this.props
         .submit(this.state.data)
-        .catch(err => this.setState({ errors: err.response.data.errors }));
+        .catch(err =>
+          this.setState({ errors: err.response.data.errors, loading: false })
+        );
     }
     // handle error cases from server
   };
@@ -48,10 +51,10 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { data, errors } = this.state;
+    const { data, errors, loading } = this.state;
 
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} loading={loading}>
         {errors.global && (
           <Message negative>
             <Message.Header>Something went wrong</Message.Header>
